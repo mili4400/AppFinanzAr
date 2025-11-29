9738b91ad7b09f05c690b08cce46a630bd798a9b
mili4400 <146014389+mili4400@users.noreply.github.com>
2025-11-29 18:46:31 -0300

    Update login_ui.py

--- ui/login_ui.py
+++ ui/login_ui.py
@@ -1,3 +1,4 @@
+# ui/login_ui.py
 import streamlit as st
 from core.auth import login_user
 
@@ -5,10 +6,9 @@ def login_screen():
     st.title("AppFinanzAr")
     st.write("Por favor, ingresa tus credenciales")
 
-    # Usamos keys únicas para evitar StreamlitDuplicateElementId
-    username = st.text_input("Usuario", key="login_username")
-    password = st.text_input("Contraseña", type="password", key="login_password")
+    username = st.text_input("Usuario", key="login_user")
+    password = st.text_input("Contraseña", type="password", key="login_pass")
 
-    # Botón con key único
-    if st.button("Login", key="login_button"):
+    if st.button("Login"):
         login_user(username, password)
+
