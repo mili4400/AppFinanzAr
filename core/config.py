9bfce4d48175757e620c889538148903c3ecd9e5
mili4400 <146014389+mili4400@users.noreply.github.com>
2025-11-29 13:43:57 -0300

    Create config.py

---
+++ core/config.py
@@ -0,0 +1,7 @@
+# core/config.py
+
+# Clave de API para EODHD
+API_KEY = "6915ae41dca776.51369114"  
+
+# Parámetros generales (opcional)
+NEWS_DAYS_BACK = 60  # cantidad de días atrás para buscar noticias
