b69008565f0ad7978f2bd6d38440db1ef71511d8
mili4400 <146014389+mili4400@users.noreply.github.com>
2025-11-29 22:38:12 -0300

    Update login_ui.py

--- ui/login_ui.py
+++ ui/login_ui.py
@@ -1,19 +1,12 @@
-# ui/login_ui.py
 import streamlit as st
 from core.auth import login_user
 
 def login_screen():
-    
-    # Título y descripción
     st.title("AppFinanzAr")
     st.write("Por favor, ingresa tus credenciales")
 
-    # Inputs con keys únicas
     username = st.text_input("Usuario", key="login_user")
     password = st.text_input("Contraseña", type="password", key="login_pass")
 
-    # Botón de login
     if st.button("Login", key="login_btn"):
         login_user(username, password)
-
-
