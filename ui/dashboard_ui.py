78335c74589fe137303af9dc05c66b6d09ccccf4
mili4400 <146014389+mili4400@users.noreply.github.com>
2025-11-30 23:08:48 -0300

    Update dashboard_ui.py

--- ui/dashboard_ui.py
+++ ui/dashboard_ui.py
@@ -11,7 +11,6 @@ from core.etf_finder import etf_screener
 from core.favorites import load_favorites, add_favorite
 from core.compare_pro import compare_indicators, compare_sentiment
 from core.utils import sma, ema, rsi
-from core.sentiment import analyze_sentiment_textblob
 
 
 # =============================
